43225c0bf9010f95271ae03695e6880be7b9e878
Alex <sanchonn@gmail.com>
2018-08-06 18:50:47 +0300

    added users, checks, tokens services, background, and logging to the file started

---
+++ test.js
@@ -0,0 +1,12 @@
+const obj = {
+    firstName : 'Sancho',
+    lastName : 'Kopytkov'
+}
+
+const str = JSON.stringify(obj);
+
+
+console.log(obj);
+console.log(str);
+console.log({}.length);
+console.log(JSON.parse({}));
